7bb860ef6fd5b8bb08c2bd7879f77e5dc2d2da77
Linmic <linmic@hey.com>
2021-10-19 15:00:27 +0900

    use prod instead

--- lib/generators/smartpay/templates/initializer.rb
+++ lib/generators/smartpay/templates/initializer.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Smartpay.configure do |config|
-  config.api_url = 'https://api.smartpay.re/smartpayments'
-  config.checkout_url = 'https://checkout.smartpay.re'
+  config.api_url = 'https://api.smartpay.co/smartpayments'
+  config.checkout_url = 'https://checkout.smartpay.co'
   config.public_key = 'pk_test_'
   config.api_secret = 'sk_test_'
 end
