994be2dcd4e63801227bd88108a1b53efe76b54d
carawang <cara4wang@gmail.com>
2021-10-22 11:44:50 +0800

    public_api_key -> public_key & private_api_key -> secret_key

--- lib/generators/smartpay/templates/initializer.rb
+++ lib/generators/smartpay/templates/initializer.rb
@@ -3,6 +3,6 @@
 Smartpay.configure do |config|
   config.api_url = 'https://api.smartpay.co/smartpayments'
   config.checkout_url = 'https://checkout.smartpay.co'
-  config.public_api_key = 'pk_test_'
-  config.private_api_key = 'sk_test_'
+  config.public_key = 'pk_test_'
+  config.secret_key = 'sk_test_'
 end
