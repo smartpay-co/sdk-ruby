a591faa552cea34c50ba58faebe3d07caf9b9c41
carawang <cara4wang@gmail.com>
2021-10-28 14:51:24 +0800

    fix: remove api url and checkout url in rails generator

--- lib/generators/smartpay/templates/initializer.rb
+++ lib/generators/smartpay/templates/initializer.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Smartpay.configure do |config|
-  config.api_url = 'https://api.smartpay.co/smartpayments'
-  config.checkout_url = 'https://checkout.smartpay.co'
   config.public_key = 'pk_test_'
   config.secret_key = 'sk_test_'
 end
