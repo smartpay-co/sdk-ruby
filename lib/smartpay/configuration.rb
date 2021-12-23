21e7565efcfb9abf22c454968d2e3e461a0a5c54
othree <othree@smartpay.co>
2021-12-23 10:46:02 +0800

    [sc-3159] Add support SMARTPAY_CHECKOUT_URL

--- lib/smartpay/configuration.rb
+++ lib/smartpay/configuration.rb
@@ -6,17 +6,21 @@ module Smartpay
     attr_writer :post_timeout, :api_url, :checkout_url
 
     DEFAULT_TIMEOUT_SETTING = 30
-    DEFAULT_API_URL = 'https://api.smartpay.co/v1'
-    DEFAULT_CHECKOUT_URL = 'https://checkout.smartpay.co'
+    DEFAULT_API_URL = "https://api.smartpay.co/v1"
+    DEFAULT_CHECKOUT_URL = "https://checkout.smartpay.co"
 
     def initialize
       @post_timeout = DEFAULT_TIMEOUT_SETTING
       @api_url = if in_development_mode?
-                   ENV['SMARTPAY_API_PREFIX'].downcase || DEFAULT_API_URL
+                   ENV["SMARTPAY_API_PREFIX"].downcase || DEFAULT_API_URL
                  else
                    DEFAULT_API_URL
                  end
-      @checkout_url = DEFAULT_CHECKOUT_URL
+      @checkout_url = if in_development_mode? && ENV["SMARTPAY_CHECKOUT_URL"].is_a?(String)
+                        ENV["SMARTPAY_CHECKOUT_URL"].downcase || DEFAULT_CHECKOUT_URL
+                      else
+                        DEFAULT_CHECKOUT_URL
+                      end
     end
 
     def post_timeout
@@ -25,7 +29,7 @@ module Smartpay
 
     def api_url
       if in_development_mode?
-        @api_url || ENV['SMARTPAY_API_PREFIX'].downcase || DEFAULT_API_URL
+        @api_url || ENV["SMARTPAY_API_PREFIX"].downcase || DEFAULT_API_URL
       else
         @api_url || DEFAULT_API_URL
       end
@@ -38,9 +42,7 @@ module Smartpay
     private
 
     def in_development_mode?
-      if ENV['SMARTPAY_API_PREFIX'].is_a?(String)
-        ENV['SMARTPAY_API_PREFIX'].downcase.include?('api.smartpay')
-      end
+      ENV["SMARTPAY_API_PREFIX"].downcase.include?("api.smartpay") if ENV["SMARTPAY_API_PREFIX"].is_a?(String)
     end
   end
 end
