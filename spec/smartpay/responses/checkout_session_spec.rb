9571e67fd3f4aa7eee0c7ba5d367912d9237e785
Cara Wang <92106570+carawangtw@users.noreply.github.com>
2022-02-09 23:13:06 +0800

    reorganize the request payload: move the keys of reference and metadata to the hash of orderData (#10)
    * test(checkout_session): merge the test cases for redirect_url
    
    * [sc-3437] put keys of reference and metadata into the hash of order data

--- spec/smartpay/responses/checkout_session_spec.rb
+++ spec/smartpay/responses/checkout_session_spec.rb
@@ -11,10 +11,24 @@ RSpec.describe Smartpay::Responses::CheckoutSession do
   end
 
   describe '#redirect_url' do
-    it do
-      expect(subject.redirect_url).to eq(
-        'https://checkout.smartpay.test/login?session-id=checkout_test_oTQpCvZzZ52UvKbrN5i4B8&public-key=pk_test_1234'
-      )
+    context 'without promotion code' do
+      it do
+        expect(subject.redirect_url).to eq(
+          'https://checkout.smartpay.test/login?session-id=checkout_test_oTQpCvZzZ52UvKbrN5i4B8&public-key=pk_test_1234'
+        )
+      end
+    end
+
+    context 'with promotion code' do
+      let(:response) { JSON.parse(File.read("./spec/fixtures/checkout_session_promotion.json"), symbolize_names: true) }
+
+      describe '#redirect_url' do
+        it do
+          expect(subject.redirect_url).to eq(
+            'https://checkout.smartpay.test/login?session-id=checkout_test_oTQpCvZzZ52UvKbrN5i4B8&public-key=pk_test_1234&promotion-code=ZOO'
+          )
+        end
+      end
     end
   end
 
@@ -25,24 +39,4 @@ RSpec.describe Smartpay::Responses::CheckoutSession do
   describe '#as_json' do
     it { expect(subject.as_json).to be_a(String) }
   end
-end
-
-RSpec.describe Smartpay::Responses::CheckoutSession do
-  subject { Smartpay::Responses::CheckoutSession.new(response) }
-
-  let(:response) { JSON.parse(File.read("./spec/fixtures/checkout_session_promotion.json"), symbolize_names: true) }
-
-  before do
-    Smartpay.configuration.checkout_url  = 'https://checkout.smartpay.test'
-    Smartpay.configuration.public_key  = 'pk_test_1234'
-  end
-
-  describe '#redirect_url' do
-    it do
-      expect(subject.redirect_url).to eq(
-        'https://checkout.smartpay.test/login?session-id=checkout_test_oTQpCvZzZ52UvKbrN5i4B8&public-key=pk_test_1234&promotion-code=ZOO'
-      )
-    end
-  end
-
 end
